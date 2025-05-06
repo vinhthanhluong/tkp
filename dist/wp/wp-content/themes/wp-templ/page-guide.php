6c9da74e4d2ea3e8f705bd9ac8a07da8d4fc0aef
Hai <alive@Hais-Mac-mini.local>
2025-05-06 16:46:39 +0700

    coding Guide

--- dist/wp/wp-content/themes/wp-templ/page-guide.php
+++ dist/wp/wp-content/themes/wp-templ/page-guide.php
@@ -7,7 +7,42 @@ include(APP_PATH . 'libs/head.php'); ?>
 <body id="guide" class="guide">
   <?php //include(APP_PATH . 'libs/header.php'); ?>
   <main id="wrap">
-
+    <div class="sec-mv">
+      <div class="content-sec">
+        <p class="img-wave-blue">
+          <img width="56" height="18" src="<?php echo createSVG(56, 18); ?>" data-src="<?php echo APP_ASSETS ?>img/common/ico_wave_blue.png" rel="js-lazy" alt="">
+        </p>
+        <p class="img-wave-yellow">
+          <img width="56" height="18" src="<?php echo createSVG(56, 18); ?>" data-src="<?php echo APP_ASSETS ?>img/common/ico_wave_yellow.png" rel="js-lazy" alt="">
+        </p>
+        <p class="img-item-1">
+          <img width="257" height="200" src="<?php echo createSVG(257, 200); ?>" data-src="<?php echo APP_ASSETS ?>img/guide/item_1.png" rel="js-lazy" alt="">
+        </p>
+        <p class="img-item-2">
+          <img width="211" height="224" src="<?php echo createSVG(211, 224); ?>" data-src="<?php echo APP_ASSETS ?>img/guide/item_2.png" rel="js-lazy" alt="">
+        </p>
+        <h1 class="tit-sec">
+          <span>SHONIN PARKについて</span>
+          <strong>About SHONIN PARK</strong>
+        </h1>
+        <p class="desc">4つのエリアで構成されているSHONIN PARK。<br>各エリアについて紹介します。</p>
+      </div>
+    </div>
+    <div class="sec-guide">
+      <div class="content-sec">
+        <div class="row-guide">
+          <div class="box-left">
+            <img width="56" height="18" src="<?php echo createSVG(56, 18); ?>" data-src="<?php echo APP_ASSETS ?>img/common/ico_wave_yellow.png" rel="js-lazy" alt="">
+          </div>
+          <div class="box-right">
+            <h2 class="tit-box">
+              <span>砂湯・温泉</span>
+              <strong>Spa Sand</strong>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
   <?php include(APP_PATH . 'libs/footer.php'); ?>
 </body>
