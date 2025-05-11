b797d804ca0d57b099dd2529f63de6ffc4b42502
thuy_dtn <thuydtn@alive-web.co.jp>
2025-05-11 01:04:05 -0700

    guide/restaurant/

--- src/js/common/smoothscroll.js
+++ src/js/common/smoothscroll.js
@@ -2,7 +2,7 @@ $(window).on("load resize", function (e) {
   $('a[href^="#"]:not([href="#"])').on("click", function (e) {
     var widthwin = $(window).width();
     var headerh = 115;
-    if (widthwin < 768) headerh = 50;
+    if (widthwin < 768) headerh = 76;
     e.preventDefault();
     var target = this.hash,
       $target = $(target),
@@ -28,7 +28,7 @@ $(function () {
 function handleAnchor(isReload = false) {
   var winWidth = $(window).width();
   var headerHeight = 115;
-  if (winWidth < 768) headerHeight = 50;
+  if (winWidth < 768) headerHeight = 76;
   var hashStr = location.hash;
   if (hashStr && hashStr != "#noback") {
     var pos = $(hashStr).offset().top - headerHeight;
