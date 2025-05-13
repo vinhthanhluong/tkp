72e08339d64810df3704541d0c693d2bcef27f77
Anh Quan <quannda@a-livesystems.com>
2025-05-13 14:25:22 +0700

    Update cms

--- dist/wp/wp-content/themes/wp-templ/functions.php
+++ dist/wp/wp-content/themes/wp-templ/functions.php
@@ -322,3 +322,40 @@ add_action('template_redirect', 'wpc_redirect_pagenum');
 
 // Disable auto redirect with same post_name
 remove_action('template_redirect', 'redirect_canonical');
+
+/**
+ * Use radio inputs instead of checkboxes for term checklists in specified taxonomies.
+ *
+ * @param   array   $args
+ * @return  array
+ */
+function wpse_139269_term_radio_checklist($args)
+{
+  if (!empty($args['taxonomy']) && ($args['taxonomy'] === 'newscat')) {
+    if (empty($args['walker']) || is_a($args['walker'], 'Walker')) { // Don't override 3rd party walkers.
+      if (! class_exists('WPSE_139269_Walker_Category_Radio_Checklist')) {
+        /**
+         * Custom walker for switching checkbox inputs to radio.
+         *
+         * @see Walker_Category_Checklist
+         */
+        class WPSE_139269_Walker_Category_Radio_Checklist extends Walker_Category_Checklist
+        {
+          function walk($elements, $max_depth, ...$args)
+          {
+            $output = parent::walk($elements, $max_depth, ...$args);
+            $output = str_replace(
+              array('type="checkbox"', "type='checkbox'"),
+              array('type="radio"', "type='radio'"),
+              $output
+            );
+            return $output;
+          }
+        }
+      }
+      $args['walker'] = new WPSE_139269_Walker_Category_Radio_Checklist;
+    }
+  }
+  return $args;
+}
+add_filter('wp_terms_checklist_args', 'wpse_139269_term_radio_checklist');
