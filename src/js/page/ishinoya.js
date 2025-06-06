fe074fac23d025469bf2da89acc5143a4d2c0004
Anh Quan <quannda@a-livesystems.com>
2025-06-06 10:19:45 +0700

    Update ishinoya page

--- src/js/page/ishinoya.js
+++ src/js/page/ishinoya.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   mvLoad();
+  jsSlideNews();
 });
 
 function mvLoad() {
@@ -11,4 +12,50 @@ function mvLoad() {
       }, 2000);
     }
   });
+}
+
+function jsSlideNews() {
+  if ($(".js-swiperNews").length) {
+    var swiperThumb = new Swiper(".js-swiperNews", {
+      spaceBetween: 50,
+      loop: true,
+      autoplay: {
+        delay: 10000,
+      },
+      pagination: {
+        el: ".info-page",
+        type: "custom",
+        renderCustom: function (swiper, current, total) {
+          function formatNumber(num) {
+            return num < 10 ? "0" + num : num;
+          }
+          return `<span class="current">${formatNumber(
+            current
+          )}</span><span class="">/ </span><span class="total">${formatNumber(
+            total
+          )}</span>`;
+        },
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+        768: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+      },
+    });
+
+    var swiper_progress = $(".js-swiperNews .swiper-progress .line");
+    swiper_progress.addClass("is-active");
+    swiperThumb.on("slideChangeTransitionStart", function () {
+      swiper_progress.removeClass("is-active");
+      // Force reflow to restart the animation
+      void swiper_progress[0].offsetWidth;
+      swiper_progress.addClass("is-active");
+      lazyloadInstance.update();
+    });
+  }
 }
