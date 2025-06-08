89e427b9ddf0137318782dea51ed577e059f1ae3
thuy_dtn <thuydtn@alive-web.co.jp>
2025-06-08 00:23:16 -0700

    page rule

---
+++ dist/wp/wp-content/themes/wp-templ/page-rule.php
@@ -0,0 +1,141 @@
+<?php
+$thisPageName = 'rule';
+include(APP_PATH . 'libs/head.php'); ?>
+<link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/page/rule.min.css?v=<?php echo APP_VER ?>">
+</head>
+
+<body id="rule">
+  <?php include(APP_PATH . 'libs/header.php'); ?>
+  <main id="wrap">
+    <div class="c-breadcrumb aos-init" data-aos="fade-up">
+      <ul>
+        <li><a href="<?php echo APP_URL; ?>">TOP</a></li>
+        <li>利用規約</li>
+      </ul>
+    </div>
+    <div class="c-mainvisual">
+      <div class="mv-inner">
+        <div class="img-wave-blue">
+          <div class="wave-top">
+            <p class="c-mask" rel="js-lazy" data-bgpc="<?php echo APP_ASSETS; ?>img/common/ico_wave_blue.png" data-bgsp="<?php echo APP_ASSETS; ?>img/common/ico_wave_blue.png"></p>
+          </div>
+          <div class="wave-bottom">
+            <p class="c-mask" rel="js-lazy" data-bgpc="<?php echo APP_ASSETS; ?>img/common/ico_wave_blue.png" data-bgsp="<?php echo APP_ASSETS; ?>img/common/ico_wave_blue.png"></p>
+          </div>
+        </div>
+        <div class="img-wave-yellow">
+          <div class="wave-top">
+            <p class="c-mask" rel="js-lazy" data-bgpc="<?php echo APP_ASSETS; ?>img/common/ico_wave_yellow.png" data-bgsp="<?php echo APP_ASSETS; ?>img/common/ico_wave_yellow.png"></p>
+          </div>
+          <div class="wave-bottom">
+            <p class="c-mask" rel="js-lazy" data-bgpc="<?php echo APP_ASSETS; ?>img/common/ico_wave_yellow.png" data-bgsp="<?php echo APP_ASSETS; ?>img/common/ico_wave_yellow.png"></p>
+          </div>
+        </div>
+        <div class="mv-ttl aos-init" data-aos="fade-up">
+          <h1 class="mv-ttl-jp">利用規約</h1>
+          <span class="mv-ttl-en">Terms of Use</span>
+        </div>
+      </div>
+    </div>
+    <div class="c-bg-wave01">
+      <div class="inner1170">
+        <ul class="rule-anchor">
+          <li>
+            <a href="#rule01" class="c-btn01 btn-anchor">
+              <i class="arr01"></i>
+              <span>SHONIN PARK 利用規約</span>
+              <i class="arr02"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#rule02" class="c-btn01 btn-anchor">
+              <i class="arr01"></i>
+              <span>ISHINOYA 別府利用規約</span>
+              <i class="arr02"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#rule03" class="c-btn01 btn-anchor">
+              <i class="arr01"></i>
+              <span>Sand SPA 利用規約</span>
+              <i class="arr02"></i>
+            </a>
+          </li>
+        </ul>
+        <div class="sec-rule" id="rule01">
+          <h2 class="rule-heading">SHONIN PARK 利用規約</h2>
+          <div class="rule-txt">
+            <p>当施設をご利用のお客様には、以下に定めた禁止行為についての規則をお守りいただくことになっております。この規則をお守りいただけない場合は、ご利用を禁止させていただく場合がございます。</p>
+            <p>(1)大きな声や音を出して騒ぐ行為。<br>(2)所定場所以外へのゴミ廃棄。<br>(3)周辺の近隣住民に迷惑及び嫌悪感を与える行為。<br>(4)次のものを持ち込む行為。</p>
+            <ul class="list-dot">
+              <li>動物類・鳥類等 (盲導犬などは除く)</li>
+              <li>著しく悪臭を発するもの</li>
+              <li>著しく多量な物品</li>
+              <li>火薬や揮発油など、発火あるいは引火しやすいもの</li>
+              <li>適法に所持を許可されていない銃砲、刀剣類</li>
+              <li>大麻、麻薬、覚せい剤等</li>
+            </ul>
+            <p>(5)賭博および風紀をみだすような行為。<br>(6)外来客を引き入れる行為。<br>(7)設備、什器備品の移動、持ち出し、目的以外の用途に利用する行為。<br>(8)泥酔状態での利用。<br>(9)そのほか、公序良俗に反する行為。</p>
+            <p>その他の注意事項</p>
+            <ul class="list-dot">
+              <li>時間にかかわらず近隣のご迷惑になるような行為(騒音、振動、臭気など)はおやめ下さい。利用状況により近隣の方へのご迷惑になった場合、即時ご退去いただく場合もありますので予めご承知おき下さい。</li>
+              <li>お客様の故意又は過失により建物や備品を壊してしまった場合は、お客様にご負担いただきますことを予めご了承下さい。</li>
+              <li>持参した食品・飲料の管理はお客様の責任でお願いいたします。また、食中毒につきまして当施設は一切責任を負いません。</li>
+            </ul>
+            <p class="desc">何卒ご理解を賜りますよう宜しくお願い申し上げます。</p>
+          </div>
+        </div>
+        <div class="sec-rule" id="rule02">
+          <h2 class="rule-heading">ISHINOYA 別府 利用規約</h2>
+          <div class="rule-txt">
+            <p>当館をご利用のお客様には、宿泊約款に基づき以下に定めた禁止行為についての規則をお守りいただくことになっております。</p>
+            <p>(1)火災の原因となる火器などの使用。<br>(2)当館所定の場所以外での喫煙。<br>(3)周辺の近隣住民に迷惑及び嫌悪感を与える行為。<br>(4)次のものを持ち込む行為。</p>
+            <ul class="list-dot">
+              <li>動物類・鳥類等 (盲導犬などは除く)</li>
+              <li>著しく悪臭を発するもの</li>
+              <li>著しく多量な物品</li>
+              <li>火薬や揮発油など、発火あるいは引火しやすいもの</li>
+              <li>適法に所持を許可されていない銃砲、刀剣類</li>
+              <li>大麻、麻薬、覚せい剤等</li>
+            </ul>
+            <p>(5)賭博および風紀をみだすような行為。<br>(6)外来客を引き入れる行為。<br>(7)設備、什器備品の移動、持ち出し、目的以外の用途に利用する行為。<br>(8)泥酔状態での利用。<br>(9)そのほか、公序良俗に反する行為。</p>
+            <p>その他の注意事項</p>
+            <ul class="list-dot">
+              <li>時間にかかわらず近隣のご迷惑になるような行為(騒音、振動、臭気、煙など)はおやめ下さい。利用状況により近隣の方へのご迷惑になった場合、即時ご退去いただく場合もありますので予めご承知おき下さい。</li>
+              <li>お客様の故意又は過失により建物や備品を壊してしまった場合は、お客様にご負担いただきますことを予めご了承下さい。</li>
+            </ul>
+            <p class="desc">何卒ご理解を賜りますよう宜しくお願い申し上げます。</p>
+            <p>TKPホテル&リゾートの宿泊約款に関しましては<a href="#">こちら</a>にてご確認をお願いいたします。</p>
+          </div>
+        </div>
+        <div class="sec-rule" id="rule03">
+          <h2 class="rule-heading">Sand SPA 利用規約</h2>
+          <div class="rule-txt">
+            <p>当施設をご利用のお客様には、以下に定めた禁止行為および健康状態についての規則をお守りいただくことになっております。この規則をお守りいただけない場合、もしくは以下の健康状態に該当する場合は、ご利用を禁止させていただく場合がございます。</p>
+            <p>(1)医師から入浴を禁じられている方。<br>(2)体調が万全ではない方。<br>(3)禁忌症急性疾患、活動性の結核、悪性腫瘍、重い心臓病、呼吸不全、髓不全又は腎不全、出血性疾患、皮膚疾患、その他一般に病勢進行中の疾患に罹患している方。<br>(4)当施設指定の砂湯専用浴衣以外でのご利用。<br>(5)故意に砂湯の砂を落とさずに屋外または当施設に戻られる方。<br>(6)当施設スタッフの案内に従わない行為。<br>(7)泥酔状態での入浴。<br>(8)次のものを持ち込む行為。</p>
+            <ul class="list-dot">
+              <li>動物類・鳥類等 (盲導犬などは除く)</li>
+              <li>著しく悪臭を発するもの</li>
+              <li>著しく多量な物品</li>
+              <li>火薬や揮発油など、発火あるいは引火しやすいもの</li>
+              <li>適法に所持を許可されていない銃砲、刀剣類</li>
+              <li>大麻、麻薬、覚せい剤等</li>
+            </ul>
+            <p>(9)他のご利用客に迷惑及び嫌悪感を与える行為。<br>(10)設備、什器備品の移動、持ち出し、目的以外の用途に利用する行為。<br>(11)そのほか、公序良俗に反する行為。</p>
+            <p>その他の注意事項</p>
+            <ul class="list-dot">
+              <li>上記の健康状態に該当すると判断した時は、当施設はお客様のご利用をお断りできるものとします。この場合は、当社からお客様に対するご利用料金の返金は行いません。</li>
+              <li>当施設の利用をもって、お客様は上記の健康状態に該当しないことを表明されたものと致します。また、当施設のご利用はお客様ご自身の判断で行うものであり、体調の異変等に関して当社が責任を追うものではございません。</li>
+              <li>時間にかかわらず近隣のご迷惑になるような行為(騒音、振動、臭気、煙など)はおやめ下さい。利用状況により近隣の方へのご迷惑になった場合、即時ご退去いただく場合もありますので予めご承知おき下さい。</li>
+              <li>お客様の故意又は過失により建物や備品を壊してしまった場合は、お客様にご負担いただきますことを予めご了承下さい。</li>
+            </ul>
+            <p class="desc">何卒ご理解を賜りますよう宜しくお願い申し上げます。</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <?php include(APP_PATH . 'libs/footer.php'); ?>
+</body>
+
+</html>
