bcb72af6eeb3f1a755313420340872113880e715
thuy_dtn <thuydtn@alive-web.co.jp>
2025-06-10 21:06:16 -0700

    /guide/ishinoya/contact/

---
+++ dist/page/guide/ishinoya/contact/index.php
@@ -0,0 +1,107 @@
+<?php
+$thisPageName = 'ishinoya-contact';
+include(dirname(__DIR__) . '/../../../app_config.php');
+include(APP_PATH . 'libs/head.php'); ?>
+<link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/page/ishinoya_contact.min.css?v=<?php echo APP_VER ?>">
+</head>
+
+<body id="ishinoya-contact">
+  <?php include(APP_PATH . 'libs/header02.php'); ?>
+  <main id="wrap">
+    <section class="c-keyvisual aos-init" data-aos="fade-up" rel="js-lazy" data-bgpc="<?php echo APP_ASSETS; ?>img/ishinoya/contact/mv.jpg" data-bgsp="<?php echo APP_ASSETS; ?>img/ishinoya/contact/mv_sp.jpg">
+      <div class="inner1170">
+        <h1 class="keyvisual-ttl">
+          <span class="ttl-en">Contact</span>
+          <span class="ttl-jp">お問い合わせ</span>
+        </h1>
+        <div class="c-breadcrumb is-breadcrumb-white">
+          <ul>
+            <li><a href="<?php echo APP_URL; ?>">TOP</a></li>
+            <li><a href="<?php echo APP_URL; ?>guide/">施設案内</a></li>
+            <li><a href="<?php echo APP_URL; ?>guide/ishinoya/">ISHINOYA 別府 / 石のや 別府</a></li>
+            <li>お問い合わせ</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <div class="sec-contact-head aos-init" data-aos="fade-up">
+      <div class="inner1170">
+        <p class="c-contact-desc">下記の「<a class="contact-link" href="#faq">よくあるご質問</a>」で解決しない場合は、下記フォームよりお問い合わせください。</p>
+        <ul class="btn-list">
+          <li><a href="<?php echo APP_URL; ?>guide/ishinoya/contact/form/"><span class="icn icn01">個人のお客様</span></a></li>
+          <li><a href="#"><span class="icn icn02">法人のお客様</span></a></li>
+        </ul>
+      </div>
+    </div>
+    <section class="sec-contact-faq" id="faq" rel="js-lazy" data-bgpc="<?php echo APP_ASSETS; ?>img/ishinoya/contact/pattern_bg.jpg" data-bgsp="<?php echo APP_ASSETS; ?>img/ishinoya/contact/pattern_bg_sp.jpg">
+      <div class="inner1170">
+        <h3 class="c-ttl04 aos-init" data-aos="fade-up">
+          <span class="c-ttl04__jp">よくあるご質問</span>
+          <span class="c-ttl04__en">Q&A</span>
+        </h3>
+        <p class="c-contact-desc aos-init" data-aos="fade-up">ISHINOYA 別府の利⽤についてよくあるご質問をまとめています。<br>こちらで確認できない内容は<a class="contact-link" href="<?php echo APP_URL; ?>guide/ishinoya/contact/form/">お問い合わせフォーム</a>よりご質問ください。</p>
+        <ul class="faq-list">
+          <li class="faq-item aos-init" data-aos="fade-up">
+            <h3 class="faq-question js-accordion">キャンセル規定について教えてください</h3>
+            <div class="faq-accordion js-accordion-content">
+              <div class="faq-answer">
+                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </div>
+          </li>
+          <li class="faq-item aos-init" data-aos="fade-up">
+            <h3 class="faq-question js-accordion">ドローンは使用できますか？</h3>
+            <div class="faq-accordion js-accordion-content">
+              <div class="faq-answer">
+                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </div>
+          </li>
+          <li class="faq-item aos-init" data-aos="fade-up">
+            <h3 class="faq-question js-accordion">定休日はありますか？</h3>
+            <div class="faq-accordion js-accordion-content">
+              <div class="faq-answer">
+                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </div>
+          </li>
+          <li class="faq-item aos-init" data-aos="fade-up">
+            <h3 class="faq-question js-accordion">ペットは同伴可能ですか？</h3>
+            <div class="faq-accordion js-accordion-content">
+              <div class="faq-answer">
+                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </div>
+          </li>
+          <li class="faq-item aos-init" data-aos="fade-up">
+            <h3 class="faq-question js-accordion">赤ちゃん用のおむつ替えスペースはありますか？</h3>
+            <div class="faq-accordion js-accordion-content">
+              <div class="faq-answer">
+                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </div>
+          </li>
+          <li class="faq-item aos-init" data-aos="fade-up">
+            <h3 class="faq-question js-accordion">Wifiはありますか？</h3>
+            <div class="faq-accordion js-accordion-content">
+              <div class="faq-answer">
+                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </div>
+          </li>
+          <li class="faq-item aos-init" data-aos="fade-up">
+            <h3 class="faq-question js-accordion">海水浴はできますか？</h3>
+            <div class="faq-accordion js-accordion-content">
+              <div class="faq-answer">
+                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
+  <?php include(APP_PATH . 'libs/footer.php'); ?>
+</body>
+
+</html>
