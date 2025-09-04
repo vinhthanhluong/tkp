7da2e54b340720f059b68584db8127421f900de2
Anh Quan <quannda@a-livesystems.com>
2025-09-04 13:49:35 +0700

    Update mod park

--- dist/wp/wp-content/themes/wp-templ/libs/mod_park.php
+++ dist/wp/wp-content/themes/wp-templ/libs/mod_park.php
@@ -1,8 +1,7 @@
 <div class="mod-park">
   <div class="mod-park__inner">
     <div class="park-bg aos-init" data-aos="fade-up" data-aos-delay="0">
-      <img class="pc" src="<?php echo createSVG(1170, 668) ?>" data-src="<?php echo APP_ASSETS; ?>img/common/map_park.svg" rel="js-lazy" width="1170" height="668" alt="SHONIN PARK マップ">
-      <img class="sp" src="<?php echo createSVG(361, 206) ?>" data-src="<?php echo APP_ASSETS; ?>img/common/map_park_sp.png" rel="js-lazy" width="361" height="206" alt="SHONIN PARK マップ">
+      <img src="<?php echo createSVG(1170, 668) ?>" data-src="<?php echo APP_ASSETS; ?>img/common/map_park.svg" rel="js-lazy" width="1170" height="668" alt="SHONIN PARK マップ">
     </div>
     <div class="mod-park__btn bnt01 aos-init" data-aos="fade-up" data-aos-anchor=".park-bg" data-aos-delay="300">
       <h3 class="btn-link js-btnpopup" data-popup="modal01">
