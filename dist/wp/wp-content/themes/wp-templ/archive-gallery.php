24e7bd58ac8eb19509cd34fbf94312462df25930
thanhlv <vinhthanhluong@gmail.com>
2025-12-07 12:41:15 +0700

    set popup gallery

--- dist/wp/wp-content/themes/wp-templ/archive-gallery.php
+++ dist/wp/wp-content/themes/wp-templ/archive-gallery.php
@@ -2,6 +2,7 @@
 $thisPageName = 'ishinoya-gallery';
 
 include(APP_PATH . 'libs/head02.php'); ?>
+<link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/lib/swiper-bundle.min.css?v=<?php echo APP_VER ?>">
 <link rel="stylesheet" href="<?php echo APP_ASSETS ?>css/page/ishinoya_gallery.min.css?v=<?php echo APP_VER ?>">
 </head>
 
@@ -259,8 +260,105 @@ include(APP_PATH . 'libs/head02.php'); ?>
         </div>
       </div>
     </div>
+
+    <div class="popup-gallery">
+      <div class="pg-shawdo"></div>
+      <div class="pg-close"></div>
+      <div class="pg-wrap">
+        <div class="pg-head">
+          <ul class="pg-head-list">
+            <li class="active">施設・設備</li>
+            <li>お部屋</li>
+            <li>温泉・砂湯</li>
+            <li>お食事</li>
+            <li>周辺</li>
+            <li>その他</li>
+          </ul>
+        </div>
+        <div class="pg-tab">
+          <div class="pg-cnt">
+            <div class="pg-sld" id="pg-sld1">
+              <div class="swiper">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <p class="img"><img src="<?php echo APP_ASSETS ?>/img/ishinoya/gallery/gallery_img01.jpg" width="380" height="280" alt="写真の紹介が入ります写真の紹介が入ります"></p>
+                    <p class="pg-tt">写真の紹介が入ります写真の紹介が入ります</p>
+                  </div>
+                  <div class="swiper-slide">
+                    <p class="img"><img src="<?php echo APP_ASSETS ?>/img/ishinoya/gallery/gallery_img01.jpg" width="380" height="280" alt="写真の紹介が入ります写真の紹介が入ります"></p>
+                    <p class="pg-tt">写真の紹介が入ります写真の紹介が入ります2</p>
+                  </div>
+                  <div class="swiper-slide">
+                    <p class="img"><img src="<?php echo APP_ASSETS ?>/img/ishinoya/gallery/gallery_img01.jpg" width="380" height="280" alt="写真の紹介が入ります写真の紹介が入ります"></p>
+                    <p class="pg-tt">写真の紹介が入ります写真の紹介が入ります3</p>
+                  </div>
+                </div>
+
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+              </div>
+            </div>
+
+            <div class="pg-thumb">
+              <div class="pg-thumb-sc" id="pg-thumb1">
+                <div class="swiper">
+                  <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                      <p class="img"><img src="<?php echo APP_ASSETS ?>/img/ishinoya/gallery/gallery_img01.jpg" width="380" height="280" alt="写真の紹介が入ります写真の紹介が入ります"></p>
+                    </div>
+                    <div class="swiper-slide">
+                      <p class="img"><img src="<?php echo APP_ASSETS ?>/img/ishinoya/gallery/gallery_img01.jpg" width="380" height="280" alt="写真の紹介が入ります写真の紹介が入ります"></p>
+                    </div>
+                    <div class="swiper-slide">
+                      <p class="img"><img src="<?php echo APP_ASSETS ?>/img/ishinoya/gallery/gallery_img01.jpg" width="380" height="280" alt="写真の紹介が入ります写真の紹介が入ります"></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
   <?php include(APP_PATH . 'libs/footer02.php'); ?>
+  <script src="<?php echo APP_ASSETS ?>js/lib/swiper-bundle.min.js?v=<?php echo APP_VER ?>"></script>
+  <script>
+    $(function() {
+      $("[id^='pg-sld']").each(function() {
+        var id = this.id.replace("pg-sld", "");
+        const thumbSelector = `#pg-thumb${id} .swiper`;
+        const mainSelector = `#pg-sld${id} .swiper`;
+
+        // Thumbnail Swiper
+        var thumbSwiper = new Swiper(thumbSelector, {
+          loop: true,
+          spaceBetween: 12,
+          slidesPerView: 'auto',
+          // width: '112px',
+          freeMode: true,
+          watchSlidesProgress: true,
+        });
+
+        // Main Swiper
+        var mainSwiper = new Swiper(mainSelector, {
+          loop: true,
+          spaceBetween: 10,
+          effect: 'fade',
+          fadeEffect: {
+            crossFade: true
+          },
+          navigation: {
+            nextEl: `#pg-sld${id} .swiper-button-next`,
+            prevEl: `#pg-sld${id} .swiper-button-prev`,
+          },
+          thumbs: {
+            swiper: thumbSwiper,
+          },
+        });
+      });
+    });
+  </script>
 </body>
 
 </html>
